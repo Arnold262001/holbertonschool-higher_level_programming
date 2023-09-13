7fc37d40687c371f4b0e7c82e25af3849615136c
888Leonidas888 <jhonny14_1@hotmail.com>
2023-09-13 04:24:41 -0700

    Se corrige 1-last_digit.py

--- python-if_else_loops_functions/1-last_digit.py
+++ python-if_else_loops_functions/1-last_digit.py
@@ -5,14 +5,14 @@ lastDigit = abs(number) % 10
 START_TXT = "Last digit of"
 lastTxt = ''
 
+if number < 0:
+   lastDigit = lastDigit * -1
+
 if (lastDigit < 6) & (lastDigit != 0):
     lastTxt = "and is less than 6 and not 0"
 elif lastDigit > 5:
     lastTxt = "and is greater than 5"
 elif lastDigit == 0:
     lastTxt = "and is 0"
-
-if number < 0:
-    lastDigit = lastDigit * -1
 
 print(f"{START_TXT} {number} is {lastDigit} {lastTxt}")
