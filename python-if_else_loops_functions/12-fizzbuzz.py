939078252b23b474f47f35f9f38bc6778a1143bd
888Leonidas888 <jhonny14_1@hotmail.com>
2023-09-14 22:00:21 -0700

    est función imprime FizzBuzz

---
+++ python-if_else_loops_functions/12-fizzbuzz.py
@@ -0,0 +1,18 @@
+#!/usr/bin/python3
+def fizzbuzz():
+    str = ''
+    tmp = ''
+
+    for i in range(1, 101):
+        if (i % 3 == 0) & (i % 5 ==0):
+            str ='FizzBuzz'
+        elif i % 5 == 0:
+            str = 'Buzz'
+        elif i % 3 == 0:
+            str = 'Fizz'
+        else:
+            str = i
+    
+        tmp = "{0} {1}".format(tmp, str)
+
+    print(tmp)
