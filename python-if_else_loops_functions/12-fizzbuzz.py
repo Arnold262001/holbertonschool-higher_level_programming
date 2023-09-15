39f085296e45198f110f20cee53038c0e887b027
Jhony Reder Escriba Bautista <98334376+888Leonidas888@users.noreply.github.com>
2023-09-15 00:28:47 -0500

    Update 12-fizzbuzz.py

--- python-if_else_loops_functions/12-fizzbuzz.py
+++ python-if_else_loops_functions/12-fizzbuzz.py
@@ -2,9 +2,9 @@
 def fizzbuzz():
     for i in range(1, 101):
         str = i
-        
-        if (i % 3 == 0) & (i % 5 ==0):
-            str ='FizzBuzz'
+
+        if (i % 3 == 0) & (i % 5 == 0):
+            str = 'FizzBuzz'
         elif i % 5 == 0:
             str = 'Buzz'
         elif i % 3 == 0:
