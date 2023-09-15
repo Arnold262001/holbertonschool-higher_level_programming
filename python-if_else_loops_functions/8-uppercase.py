2e80d0d7a15da45d7b96f226d60cb555cc45b622
888Leonidas888 <jhonny14_1@hotmail.com>
2023-09-14 18:30:34 -0700

    Se corrige 8-uppercase.py

--- python-if_else_loops_functions/8-uppercase.py
+++ python-if_else_loops_functions/8-uppercase.py
@@ -3,7 +3,10 @@ def uppercase(str):
     for i in str:
         number = int(ord(i))
 
-        if 65 <= number <= 90 or 48 <= number <= 57 or number == 32:
+        if 97 <= number <= 122:
+            number -= 32
+
+        if (len(str) - 1) != i:
             print("{0}".format(chr(number)), end='')
         else:
-            print("{0}".format(chr(number - 32)), end='')
+            print("{0}".format(chr(number)))
