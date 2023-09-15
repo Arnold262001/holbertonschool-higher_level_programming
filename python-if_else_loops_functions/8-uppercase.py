d1e9e240267eba345695592a7a96e49b4707ba53
Jhony Reder Escriba Bautista <98334376+888Leonidas888@users.noreply.github.com>
2023-09-15 00:40:36 -0500

    Update 8-uppercase.py

--- python-if_else_loops_functions/8-uppercase.py
+++ python-if_else_loops_functions/8-uppercase.py
@@ -2,19 +2,17 @@
 def uppercase(str):
     o = 0
 
-    if str == "":
-        str = ' '
+    if str != "":
+        for i in str:
+            number = int(ord(i))
 
-    for i in str:
-        number = int(ord(i))
+            if 97 <= number <= 122:
+                number -= 32
 
-        if 97 <= number <= 122:
-            number -= 32
-
-        if (len(str) - 1) != o:
-            print("{0}".format(chr(number)), end='')
-        else:
-            break
-        o += 1
+            if (len(str) - 1) != o:
+                print("{0}".format(chr(number)), end='')
+            else:
+                break
+            o += 1
 
     print("{0}".format(chr(number)))
