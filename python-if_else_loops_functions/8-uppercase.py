5b7aa943f2a266a4d91fb507d1fa9017fd974b93
Jhony Reder Escriba Bautista <98334376+888Leonidas888@users.noreply.github.com>
2023-09-16 11:02:45 -0500

    Update 8-uppercase.py

--- python-if_else_loops_functions/8-uppercase.py
+++ python-if_else_loops_functions/8-uppercase.py
@@ -1,16 +1,16 @@
 #!/usr/bin/python3
 def uppercase(str):
+    if str == "":
+        str = " "
+        
     lenStr = len(str)
 
-    if lenStr != 0:
-        for i in range(lenStr):
-            number = ord(str[i])
+    for i in range(lenStr):
+        number = ord(str[i])
 
-            if 97 <= number <= 122:
-                number -= 32
+        if 97 <= number <= 122:
+            number -= 32
 
-            if i < lenStr - 1:
-                print("{0}".format(chr(number)), end='')
-    else:
-        number = None
+        if i < lenStr - 1:
+            print("{0}".format(chr(number)), end='')
     print("{0}".format(chr(number)))
