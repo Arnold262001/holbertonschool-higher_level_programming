6b600c0af05eab444c71effe7c50ffabdfcd5780
Jhony Reder Escriba Bautista <98334376+888Leonidas888@users.noreply.github.com>
2023-09-16 15:58:56 -0500

    Update 8-uppercase.py

--- python-if_else_loops_functions/8-uppercase.py
+++ python-if_else_loops_functions/8-uppercase.py
@@ -1,4 +1,4 @@
 #!/usr/bin/python3
 def uppercase(str):
-    n = [chr(ord(l) - 32) if 'a' <= l <= 'z' else l for l in str]
+    n = [chr(ord(c) - 32) if 'a' <= c <= 'z' else c for c in str]
     print("{0}".format("".join(n)))
