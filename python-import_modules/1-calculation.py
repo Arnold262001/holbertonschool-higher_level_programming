97a558c4fdf732f4057cf313761bcc883be58e4e
888Leonidas888 <jhonny14_1@hotmail.com>
2023-09-18 22:28:01 -0700

    Actuliza 1-calculation.py

--- python-import_modules/1-calculation.py
+++ python-import_modules/1-calculation.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 
-from calculator_1 import add, sub, mul, div
+import calculator_1
 
 a = 10
 b = 5
@@ -8,5 +8,5 @@ b = 5
 if __name__ == '__main__':
     print("{0} + {1} = {2}".format(a, b, calculator_1.add(a, b)))
     print("{0} - {1} = {2}".format(a, b, calculator_1.sub(a, b)))
-    print("{0} * {1} = {2}".format(a, b,calculator_1. mul(a, b)))
+    print("{0} * {1} = {2}".format(a, b, calculator_1.mul(a, b)))
     print("{0} / {1} = {2}".format(a, b, calculator_1.div(a, b)))
