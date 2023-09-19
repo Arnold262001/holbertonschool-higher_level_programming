ecc449ccf09b7a16734a596e72176e391a13ca2e
Jhony Reder Escriba Bautista <98334376+888Leonidas888@users.noreply.github.com>
2023-09-19 00:30:40 -0500

    Actualiza 1-calculation.py

--- python-import_modules/1-calculation.py
+++ python-import_modules/1-calculation.py
@@ -1,12 +1,12 @@
 #!/usr/bin/python3
 
-import calculator_1
+from calculator_1 import add, sub, mul, div
 
 a = 10
 b = 5
 
 if __name__ == '__main__':
-    print("{0} + {1} = {2}".format(a, b, calculator_1.add(a, b)))
-    print("{0} - {1} = {2}".format(a, b, calculator_1.sub(a, b)))
-    print("{0} * {1} = {2}".format(a, b, calculator_1.mul(a, b)))
-    print("{0} / {1} = {2}".format(a, b, calculator_1.div(a, b)))
+    print("{0} + {1} = {2}".format(a, b, add(a, b)))
+    print("{0} - {1} = {2}".format(a, b, sub(a, b)))
+    print("{0} * {1} = {2}".format(a, b, mul(a, b)))
+    print("{0} / {1} = {2}".format(a, b, div(a, b)))
