fb78bf3911798591236307494751a4a0a6de149f
Jhony Reder Escriba Bautista <98334376+888Leonidas888@users.noreply.github.com>
2023-09-22 15:49:35 -0500

    Update 6-print_matrix_integer.py

--- python-data_structures/6-print_matrix_integer.py
+++ python-data_structures/6-print_matrix_integer.py
@@ -2,13 +2,12 @@
 def print_matrix_integer(matrix=[[]]):
     if matrix == [[]]:
         print()
-        exit()
-
-    for i in matrix:
-        u = 1
-        for o in i:
-            if u < len(i):
-                print("{} ".format(o), end='')
-            else:
-                print("{}".format(o))
-            u += 1
+    else:
+        for i in matrix:
+            u = 1
+            for o in i:
+                if u < len(i):
+                    print("{} ".format(o), end='')
+                else:
+                    print("{}".format(o))
+                u += 1
