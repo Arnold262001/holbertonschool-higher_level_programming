61a04bcedfb021704238676229f610bebd3c01b8
Jhony Reder Escriba Bautista <98334376+888Leonidas888@users.noreply.github.com>
2023-09-22 17:00:21 -0500

    Update 4-new_in_list.py

--- python-data_structures/4-new_in_list.py
+++ python-data_structures/4-new_in_list.py
@@ -2,6 +2,5 @@
 def new_in_list(my_list, idx, element):
     n = my_list[:]
     if idx >= 0 and idx < len(my_list):
-        del(n[idx])
-        n.insert(idx, element)
+        n[idx] = element
     return n
