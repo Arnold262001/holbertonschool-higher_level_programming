4cdf157e324068e213ef16eb30a0c5cba0c8b83f
Jhony Reder Escriba Bautista <98334376+888Leonidas888@users.noreply.github.com>
2023-09-22 23:41:04 -0500

    Update 10-divisible_by_2.py

--- python-data_structures/10-divisible_by_2.py
+++ python-data_structures/10-divisible_by_2.py
@@ -1,10 +1,9 @@
 #!/usr/bin/python3
 def divisible_by_2(my_list=[]):
       n = []
-      if len(my_list) > 0:
-            for i in my_list:
-                  if i % 2 == 0:
-                        n.append(True)
-                  else:
-                        n.append(False)
+      for i in my_list:
+            if i % 2 == 0:
+                  n.append(True)
+            else:
+                  n.append(False)
       return n
