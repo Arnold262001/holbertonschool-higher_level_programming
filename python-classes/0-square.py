6ef198d23a3075bcb00a9afcad2fdc4656595264
Jhony Reder Escriba Bautista <98334376+888Leonidas888@users.noreply.github.com>
2023-10-08 18:37:21 -0500

    Update 0-square.py

--- python-classes/0-square.py
+++ python-classes/0-square.py
@@ -2,11 +2,10 @@
 """
 Este módulo representa la clase Square
 """
+
+
 class Square:
     """
     Esta clase define un cuadrado
     """
     pass
-    """
-    Métodos y atributos en construcción.
-    """
