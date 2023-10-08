1b5852bc36fb00b7797fc9b2db8f9d7e5ad9a9b9
888Leonidas888 <jhonny14_1@hotmail.com>
2023-10-08 16:49:36 -0700

    se sube clase 1-square.py

---
+++ python-classes/1-square.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+"""
+Este módulo representa la clase Square
+"""
+
+
+class Square:
+    """
+    Esta clase define un cuadrado
+    """
+
+    def __init__(self, size):
+        self._size = size
