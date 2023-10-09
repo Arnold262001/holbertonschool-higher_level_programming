a74818c7b18d868a8d34c190ff65dfc2d4e5753b
888Leonidas888 <jhonny14_1@hotmail.com>
2023-10-08 21:34:35 -0700

    se carga 0-add_integer.py  y su archivo de prueba

---
+++ python-test_driven_development/0-add_integer.py
@@ -0,0 +1,22 @@
+#!/usr/bin/python3
+"""
+Este módulo contiene operaciones matemáticas
+"""
+
+
+def add_integer(a, b=98):
+    """
+    Esta función suma dos enteros.
+
+    Args:
+        a (int): Primer entero
+        b (int): Segundo entero(valor por defecto 98)
+    """
+
+    if type(a) != int and type(a) != float:
+        raise TypeError("a must be an integer")
+
+    if type(b) != int and type(b) != float:
+        raise TypeError("b must be an integer")
+
+    return int(a) + int(b)
