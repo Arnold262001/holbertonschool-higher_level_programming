998a1faa3a4b93278172844551c6de9ef9b4b790
Jhony Reder Escriba Bautista <98334376+888Leonidas888@users.noreply.github.com>
2023-10-10 23:07:43 -0500

    Update 2-safe_print_list_integers.py

--- python-exceptions/2-safe_print_list_integers.py
+++ python-exceptions/2-safe_print_list_integers.py
@@ -9,6 +9,6 @@ def safe_print_list_integers(my_list=[], x=0):
                 print("{:d}".format(e), end='')
                 n_element += 1
     except IndexError as e:
-        print(e)
+        continue
     print()
     return n_element
