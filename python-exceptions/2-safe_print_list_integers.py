3a0d48af30cdb68d2ef45ae81ccdc4f40ed590df
Jhony Reder Escriba Bautista <98334376+888Leonidas888@users.noreply.github.com>
2023-10-10 23:10:38 -0500

    Update 2-safe_print_list_integers.py

--- python-exceptions/2-safe_print_list_integers.py
+++ python-exceptions/2-safe_print_list_integers.py
@@ -1,14 +1,11 @@
 #!/usr/bin/python3
 def safe_print_list_integers(my_list=[], x=0):
     n_element = 0
-
-    try:
-        for e in range(x):
-            e = my_list[e]
-            if type(e) == int:
-                print("{:d}".format(e), end='')
-                n_element += 1
-    except (IndexError, ValueError):
-        continue
+    for i in range(0, x):
+        try:
+            print("{:d}".format(my_list[i]), end="")
+            n_element += 1
+        except (ValueError, TypeError):
+            continue
     print()
     return n_element
