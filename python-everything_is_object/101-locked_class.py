4346efa92dc6e8be6c0822a054adbc387310ef84
Jhony <Jhonny14_1@hotmail.com>
2023-10-16 17:40:34 -0500

    Se actualiza 101-locked_class.py

--- python-everything_is_object/101-locked_class.py
+++ python-everything_is_object/101-locked_class.py
@@ -1,8 +1,22 @@
 #!/usr/bin/python3
+"""
+Este módulo define una clase
+"""
+
+
 class LockedClass:
+    """
+    Esta clase define a LockedClass
+    """
+
     __slots__ = ['first_name']
 
     def __setattr__(self, name, value):
+        """
+        Este metodo mágico sirve para crear un atributo especifico de
+        forma dinámica y lanza un error de no serlo.
+        """
+
         if name != 'first_name':
             raise AttributeError(
                 f"'LockedClass' object has no attribute '{name}'")
