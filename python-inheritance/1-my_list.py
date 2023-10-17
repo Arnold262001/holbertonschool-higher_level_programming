24a20a90eba35cc2327e4be3ddb26d6758fed6c2
Jhony <Jhonny14_1@hotmail.com>
2023-10-17 18:00:26 -0500

    Se carga 1-my_list y tests/1-my_list.txt

---
+++ python-inheritance/1-my_list.py
@@ -0,0 +1,24 @@
+# #!/usr/bin/python3
+"""
+Este módulo define una clase que hereda de list
+"""
+
+
+class MyList(list):
+    """
+    Esta clase toma la funcionalidad heredando de list.
+    """
+
+    def print_sorted(self):
+        """
+        Esta función imprime una copia de la lista ordenada.
+        """
+
+        print(sorted(self))
+
+    def __repr__(self):
+        """
+        Este métodod regresa un string, representación de lista
+        """
+
+        return f"{list(self)}"
