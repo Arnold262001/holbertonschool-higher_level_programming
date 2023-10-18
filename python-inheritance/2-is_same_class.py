e5dc31ecb0aa337605fc597ad5728497fa0e0413
Jhony <Jhonny14_1@hotmail.com>
2023-10-17 22:07:04 -0500

    Se carga 3-is_kind_of_class.py y se actualiza 2-is_same_class.py

--- python-inheritance/2-is_same_class.py
+++ python-inheritance/2-is_same_class.py
@@ -1,17 +1,17 @@
 #!/usr/bin/python3
 """
-Este módulo contiene la función para validar instancias.
+Este módulo contiene la función para validar el tipo de dato
 """
 
 
 def is_same_class(obj, a_class):
     """
-    Esta función verifica si un objeto perteneca a una clase.
+    Esta función verifica si un objeto es un tipo de dato en concreto.
 
     Args:
         obj (objeto): El objeto que se desea validar.
 
     Returns:
-        True (bool): Sí obj pertenece a a_class, caso contrario False
+        True (bool): Sí es el mismo tipo de dato de a_class, False caso contrario.
     """
     return type(obj) == a_class
