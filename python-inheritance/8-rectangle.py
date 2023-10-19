ca8183b23fc6a06fcc8aedd433a5b72c8e8deca7
Jhony <Jhonny14_1@hotmail.com>
2023-10-19 00:00:57 -0500

    Se carga 8-rectangle.py

--- python-inheritance/8-rectangle.py
+++ python-inheritance/8-rectangle.py
@@ -38,6 +38,18 @@ class BaseGeometry:
 
 
 class Rectangle(BaseGeometry):
+    """
+    Esta clase define un Rectangle
+    """
+
     def __init__(self, width, height):
+        """
+        Este metodo inicializa con el ancho y alto del rectangulo
+
+        Args:
+            width (int): Ancho del rectangulo.
+            height (int): Alto del rectangulo.
+        """
+
         self.__width = width
         self.__height = height
