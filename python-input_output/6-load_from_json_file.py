841383778e3457b158593bda60b391d3ab0f939a
Jhony <Jhonny14_1@hotmail.com>
2023-10-22 09:53:14 -0500

    Se carga 6-load_from_json_file.py

---
+++ python-input_output/6-load_from_json_file.py
@@ -0,0 +1,22 @@
+#!/usr/python3
+"""
+Este módulo define  la función
+load_from_file, para convertir string Json a objeto python.
+"""
+import json
+
+
+def load_from_json_file(filename):
+    """
+    Esta función deserializa un el contenido de
+    un archivo enun objeto de python.
+
+    Args:
+        filename (str): Nombre del fichero donde se encuentra
+        los datos a deserializar.
+    Returns:
+        obj: Regresa un objeto de python.
+    """
+    with open(filename, 'r') as fr:
+        content = fr.read()
+        return json.loads(content)
