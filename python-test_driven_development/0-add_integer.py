b4bd0c333ae0e3a08c8b9ef20dce45efe0ca9c8a
Jhony <Jhonny14_1@hotmail.com>
2023-10-29 10:30:25 -0500

    Se actualiza 0-add_integer.py tests/0-add_integer.txt

--- python-test_driven_development/0-add_integer.py
+++ python-test_driven_development/0-add_integer.py
@@ -22,9 +22,9 @@ def add_integer(a, b=98):
     n = dict(a=a, b=b)
 
     for k, v in n.items():
-        if not isinstance(v, (int, float)):
+        if v is None or (not isinstance(v, (int, float))):
             raise TypeError(f"{k} must be an integer")
-    result = a + b
+    result = n['a'] + n['b']
     if result == float('inf') or result == -float('inf'):
         return 89
-    return int(a) + int(b)
+    return int(n['a']) + int(n['b'])
